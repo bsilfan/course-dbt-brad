e7facbdee53ddc0ad2892636e74ff1e367635f0a
bsilfan <brad@remarkablefoods.com>
2021-12-02 19:24:36 +0000

    fix dbt util

--- dbt-greenery/models/marts/product/fact_user_sessions_dbt_util.sql
+++ dbt-greenery/models/marts/product/fact_user_sessions_dbt_util.sql
@@ -5,7 +5,7 @@
 }}
 
 {% set event_types = dbt_utils.get_query_results_as_dict(
-    "select distinct event_type from" 
+    "select distinct event_type as event_type, event_type as column_name from" 
     ~ ref('stg_events')
     )
 %}
@@ -14,8 +14,8 @@ select
   created_at
   , user_id
   , session_id
-  {% for event_type in event_types %}
-  , count(distinct case when event_type = '{{event_type}}' then event_id end) as {{ event_type }}
+  {% for event_type in event_types['event_type'] %}
+  , count(distinct case when event_type = '{{event_type}}' then event_id end) as {{ event_types['event_type'][loop.index0] }}
   {% endfor %}
 from {{ ref("stg_events") }}
 group by 1, 2, 3
