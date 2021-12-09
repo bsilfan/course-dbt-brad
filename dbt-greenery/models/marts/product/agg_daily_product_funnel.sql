62f79a7825775f52b123fde87a24c24799126998
bsilfan <brad@remarkablefoods.com>
2021-12-09 01:55:06 +0000

    refactor agg_daily_product_funnel

--- dbt-greenery/models/marts/product/agg_daily_product_funnel.sql
+++ dbt-greenery/models/marts/product/agg_daily_product_funnel.sql
@@ -4,27 +4,16 @@
   )
 }}
 
-with total_sessions as (
-
 select
     d.date as date,
-    count(distinct us.session_id) as total_user_sessions
+    count(distinct us.session_id) as total_user_sessions,
+    count(distinct case when page_view > 0 then session_id end) as total_page_views,
+    count(distinct case when add_to_cart > 0 then session_id end) as total_adds_to_cart,
+    count(distinct case when checkout > 0 then session_id end) as total_checkouts,
+    count(distinct case when page_view > 0 then session_id end)::float / count(distinct us.session_id)::float  as sessions_with_page_views_pct,
+    count(distinct case when add_to_cart > 0 then session_id end)::float / count(distinct us.session_id)::float  as sessions_with_add_to_cart_pct,
+    count(distinct case when checkout > 0 then session_id end)::float / count(distinct us.session_id)::float  as sessions_with_transaction_pct
 from {{ ref("dim_dates") }} d
 left join {{ ref("fact_user_sessions") }} us on d.date = date_trunc('day', us.created_at)
 group by 1
-
-)
-
-select 
-    t.date,
-    t.total_user_sessions,
-    sum(total_page_views) as total_page_views,
-    sum(total_adds_to_cart) as total_adds_to_cart,
-    sum(has_checkout) as total_checkouts,
-    sum(total_page_views) / t.total_user_sessions as sessions_with_page_views_pct,
-    sum(total_adds_to_cart) / t.total_user_sessions as sessions_with_add_to_cart_pct,
-    sum(has_checkout)::float / t.total_user_sessions as sessions_with_transaction_pct
-from total_sessions t 
-left join {{ ref("fact_page_views") }} pv on  t.date = date_trunc('day', pv.event_date)
-group by 1, 2
-order by 1
+having count(distinct us.session_id) != 0
